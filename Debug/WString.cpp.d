061a397aeba53205b68e19afbf35a807e44c664c
Richard <rgbailey@gmail.com>
2016-08-09 21:34:32 -0700

    cleanup files

--- Debug/WString.cpp.d
+++
@@ -1,3 +0,0 @@
-C:\Users\Richard\AppData\Local\V.Micro\Arduino\Builds\Variable_intervalmeter\uno\WString.cpp.o: \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino\WString.cpp \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino\WString.h
